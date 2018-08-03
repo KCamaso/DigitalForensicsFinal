918df42c419c0734aa60cd788e17fc943d6fed23
Karen Camaso <camasok@wit.edu>
2018-08-03 12:43:51 -0400

    Added comments

--- ConsoleApp2/Program.vb
+++ ConsoleApp2/Program.vb
@@ -14,14 +14,18 @@ Module Program
         Console.WriteLine("Please input the path of where the extracted files will go (Example: 'c:\Documents\Output')")
         Dim extractPath As String = Console.ReadLine()
 
+        'Extracts docs to directorys named
         ZipFile.ExtractToDirectory(zipPath, extractPath, True)
 
+        'Opens the files for the user profiles and the actual document
+        'Needs to add checks for not found exceptions.
         Dim xr As XmlReader = XmlReader.Create(extractPath + "\docProps\core.xml")
         Dim xa As XmlReader = XmlReader.Create(extractPath + "\word\document.xml")
 
-        End If
+        ' Reads through the XML tags, pulls, the appropriate IDs from them
         Dim idEdit As New List(Of String)
         While xa.Read()
+            'w:r = enter keys
             If xa.Name = "w:r" Then
                 Dim id As String = xa("w:rsidR")
                 If id IsNot Nothing Then
@@ -29,6 +33,7 @@ Module Program
 
                 End If
             End If
+            'w:r = paragraphs written 
             If xa.Name = "w:p" Then
                 Dim id As String = xa("w:rsidR")
                 If id IsNot Nothing Then
@@ -38,6 +43,7 @@ Module Program
             End If
         End While
 
+        'Groups them all up by appearance and tallies their values
         Dim group = idEdit.GroupBy(Function(value) value)
 
 
@@ -45,11 +51,16 @@ Module Program
 
         Console.WriteLine("Edit IDs (In Order of Appearance)")
         For Each grp In group
+            'Displays each one with the number of times
             Console.WriteLine(grp(0) & " - " & grp.Count & " times")
         Next
 
         While xr.Read()
-
+            ' Displays:
+            ' Creator's name
+            ' Last Modified
+            ' Date Created
+            ' Date Last Modified
             If xr.IsStartElement("dc:creator") Then
                 Console.WriteLine("Created by: {0}", xr.ReadInnerXml())
             End If
